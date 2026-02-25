ec4fe9f0642461fe42c48a592317f96c94f20889
LuEn238 <lg230055@gmail.com>
2026-02-24 20:45:24 -0500

    Update Program.cs

--- Actividad1 UnidadII/Program.cs
+++ Actividad1 UnidadII/Program.cs
@@ -1,125 +1,143 @@
-﻿namespace Actividad1_UnidadII
+using System;
+using System.Collections.Generic;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int cantidad = 0;
+
+        // Solicitar cantidad de personas (validación)
+        while (true)
         {
+            Console.Write("Ingrese la cantidad de personas a registrar: ");
+            string entrada = Console.ReadLine();
 
-            string edad, cantidadPersonas;
-            int cantper, ed;
-            string nombre;
-            bool isValid, edIsValid;
-            List<String> Listanommen = []; 
-            List<String> Listanommay = [];
-            List<int> Listaedmen = [];
-            List<int> Listaedmay = [];
-            do
+            if (int.TryParse(entrada, out cantidad) && cantidad >= 1)
             {
-
-
-                Console.WriteLine("¿Cuantas personas vas a registrar?");
-                cantidadPersonas = Console.ReadLine();
-                //cantidadPersonas
-                try
-                {
-                    cantper = Convert.ToInt16(cantidadPersonas);
-                    isValid = false;
-                    if (cantper > 0)
-                    {
-                        if (cantper == 1)
-                        {
-                            Console.WriteLine("Escribe el nombre de la persona");
-                            nombre = Console.ReadLine();
-                            do
-                            {
-                                Console.WriteLine("Escribe la edad de la persona");
-                                edad = Console.ReadLine();
-                                try
-                                {
-                                    ed = Convert.ToInt16(edad);
-                                    edIsValid = false;
-                                    if(ed > 0)
-                                    {
-                                        if (ed >= 18)
-                                        {
-                                            Console.WriteLine(nombre + " es mayor de edad con " + edad + " años");
-                                        }
-                                        else
-                                        {
-                                            Console.WriteLine(nombre + " es menor de edad con " + edad + " años");
-                                        }
-                                    }
-                                }
-                                catch
-                                {
-                                    Console.WriteLine("valor inválido, por favor, escriba un número");
-                                    edIsValid = true;
-                                }
-                            } while (edIsValid);
-                        }
-                        else
-                        {
-                            for (int i = 1; i <= cantper; i++)
-                            {
-                                Console.WriteLine("Ingrese el nombre de la persona "+i);
-                                nombre = Console.ReadLine();
-                                do
-                                {
-                                    Console.WriteLine("Ingrese la edad de la persona " + i);
-                                    edad = Console.ReadLine();
-                                    try
-                                    {
-                                        ed = Convert.ToInt16(edad);
-                                        if (ed > 0)
-                                        {
-                                            if (ed >= 18)
-                                            {
-                                                Listanommay.Add(nombre);
-                                                Listaedmay.Add(ed);
-                                            }
-                                            else
-                                            {
-                                                Listanommen.Add(nombre);
-                                                Listaedmen.Add(ed);
-                                            }
-                                        } else
-                                        {
-                                            Console.WriteLine("Edad inválida, no se registró");
-                                        }
-                                        edIsValid = false;
-                                    }
-                                    catch
-                                    {
-                                        Console.WriteLine("valor inválido, por favor, escriba un número");
-                                        edIsValid = true;
-                                    }
-                                } while(edIsValid);
-                                
-                            }
-                            Console.WriteLine("Lista de personas mayores");
-                            for (int j = 0; j < Listaedmay.Count; j++)
-                            {
-                                Console.WriteLine(Listanommay[j] + " - " + Listaedmay[j]);
-                            }
-                            Console.WriteLine("Lista de personas menores");
-                            for (int j = 0; j < Listaedmen.Count; j++)
-                            {
-                                Console.WriteLine(Listanommen[j] + " - " + Listaedmen[j]);
-                            }
-
-                        }
-
-                    } else
-                    {
-                        Console.WriteLine("Valor inválido");
-                    }
-                }
-                catch
-                {
-                    Console.WriteLine("valor inválido, por favor, escriba un número");
-                    isValid = true;
-                }
-            } while (isValid);
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido. Debe ingresar un número entero mayor o igual a 1.");
+            }
         }
+
+        // Listas para almacenar datos
+        List<string> nombres = new List<string>();
+        List<int> edades = new List<int>();
+
+        // Captura de datos
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.WriteLine($"\nPersona #{i + 1}");
+
+            // Validar nombre (no vacío)
+            string nombre;
+            while (true)
+            {
+                Console.Write("Ingrese el nombre: ");
+                nombre = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(nombre))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("El nombre no puede estar vacío.");
+                }
+            }
+
+            // Validar edad
+            int edad;
+            while (true)
+            {
+                Console.Write("Ingrese la edad: ");
+                string entradaEdad = Console.ReadLine();
+
+                if (int.TryParse(entradaEdad, out edad) && edad >= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Edad inválida. Debe ingresar un número entero válido.");
+                }
+            }
+
+            nombres.Add(nombre);
+            edades.Add(edad);
+        }
+
+        Console.WriteLine("\n----------------------------------");
+
+        // Caso especial: solo una persona
+        if (cantidad == 1)
+        {
+            Console.WriteLine("Registro individual:");
+            Console.WriteLine($"Nombre: {nombres[0]}");
+            Console.WriteLine($"Edad: {edades[0]}");
+
+            if (edades[0] >= 18)
+                Console.WriteLine("Es MAYOR de edad.");
+            else
+                Console.WriteLine("Es MENOR de edad.");
+        }
+        else
+        {
+            // Lista general
+            Console.WriteLine("\nLista general de personas:");
+            for (int i = 0; i < nombres.Count; i++)
+            {
+                Console.WriteLine($"{nombres[i]} - {edades[i]} años");
+            }
+
+            // Clasificación
+            List<string> mayores = new List<string>();
+            List<int> edadesMayores = new List<int>();
+
+            List<string> menores = new List<string>();
+            List<int> edadesMenores = new List<int>();
+
+            for (int i = 0; i < nombres.Count; i++)
+            {
+                if (edades[i] >= 18)
+                {
+                    mayores.Add(nombres[i]);
+                    edadesMayores.Add(edades[i]);
+                }
+                else
+                {
+                    menores.Add(nombres[i]);
+                    edadesMenores.Add(edades[i]);
+                }
+            }
+
+            // Mostrar mayores
+            if (mayores.Count > 0)
+            {
+                Console.WriteLine("\nPersonas MAYORES de edad:");
+                for (int i = 0; i < mayores.Count; i++)
+                {
+                    Console.WriteLine($"{mayores[i]} - {edadesMayores[i]} años");
+                }
+            }
+
+            // Mostrar menores
+            if (menores.Count > 0)
+            {
+                Console.WriteLine("\nPersonas MENORES de edad:");
+                for (int i = 0; i < menores.Count; i++)
+                {
+                    Console.WriteLine($"{menores[i]} - {edadesMenores[i]} años");
+                }
+            }
+        }
+
+        Console.WriteLine("\nPrograma finalizado.");
+        Console.ReadKey();
     }
 }
+
+
